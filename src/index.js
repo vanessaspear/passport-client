49d6fcaa3f7f4420d63c00e11dca0bf33659f4e0
Vanessa Spear <vanessavspear@gmail.com>
2023-03-14 10:45:25 -0500

    Updated bootstrapimport to use cdn in index.html file.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { Passport } from "./components/Passport"
 import { createRoot } from "react-dom/client"
 import "./index.css"
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from "react-router-dom"
 import { NavBar } from "./components/nav/NavBar"
 

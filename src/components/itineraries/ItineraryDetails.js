f28c86b3f0d1b41832c5e8dc23ad0c03d48b0b86
Vanessa Spear <vanessavspear@gmail.com>
2023-03-16 09:37:34 -0500

    Client can now delete an itinerary from a trip

--- src/components/itineraries/ItineraryDetails.js
+++ src/components/itineraries/ItineraryDetails.js
@@ -2,11 +2,13 @@
 
 import { useState, useEffect } from "react"
 import { useParams } from "react-router-dom"
+import { useNavigate } from 'react-router-dom'
 import { Link } from "react-router-dom"
-import { getItinerary } from "../managers/ItineraryManager"
+import { deleteItinerary, getItinerary } from "../managers/ItineraryManager"
 
 export const ItineraryDetails = () => {
     const { itineraryId } = useParams()
+    const navigate = useNavigate()
     const [itinerary, setItinerary] = useState({
         name: "",
         itinerary_description: "",
@@ -15,7 +17,7 @@ export const ItineraryDetails = () => {
         end_time: "",
         city: "",
         state_or_country: "",
-        trip_id: 0,
+        trip: {},
         categories: []
     })
 
@@ -26,7 +28,17 @@ export const ItineraryDetails = () => {
 
     return <>
         <div className="row my-5 mx-5">
-            <Link to={`/itineraries/edit/${itineraryId}`} className="btn btn-primary col-3">Edit Itinerary</Link>
+            <Link to={`/itineraries/edit/${itineraryId}`} className="btn btn-primary col-3">Edit</Link>
+            <button className="btn btn-primary col-3" onClick={() => {
+                if (
+                    window.confirm(
+                      `Are you sure you want to delete this itinerary?`
+                    )
+                )
+                deleteItinerary(itinerary.id)
+                    .then(() => navigate(`/trips/${itinerary?.trip?.id}`))
+            }
+            }>Delete</button>
         </div>
         <h5>{itinerary.name}</h5>
         <p>{itinerary.itinerary_description}</p>

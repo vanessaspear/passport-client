4db4bf8cffb430a1fdd2bdaa0cb522aaa5c1fadc
Vanessa Spear <vanessavspear@gmail.com>
2023-03-17 10:13:29 -0500

    removed css styling on body element since it was affecting components outside of the landing page

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -6,19 +6,16 @@ import "./Landing.css";
 export const Landing = () => {
 
     return <>
-        <video autoPlay loop muted className="myVideo">
-            <source src={takeoffVideo} type="video/mp4" />
-        </video>
 
-    <div className="myVideoContainer">
-        <video autoPlay loop muted className="myVideo">
-            <source src={takeoffVideo} type="video/mp4" />
-        </video>
-        <div className="myText">
-            <h1>Passport</h1>
-            <p>Let Your Life Liftoff</p>
+        <div className="myVideoContainer">
+            <video autoPlay loop muted className="myVideo">
+                <source src={takeoffVideo} type="video/mp4" />
+            </video>
+            <div className="myText">
+                <h1>Passport</h1>
+                <p>Let Your Life Liftoff</p>
+            </div>
         </div>
-    </div>
         
     </>
 }

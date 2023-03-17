012cc785554395435df53de1f94ee091ac3c029c
Vanessa Spear <vanessavspear@gmail.com>
2023-03-17 09:44:19 -0500

    Added a landing page background video

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,7 +1,13 @@
+// Landing page for when users first access website
+
+import takeoffVideo from "./assets/airplane_takeoff.mp4"
+
 export const Landing = () => {
 
     return <>
-        Passport: Travel the World!
+        <video autoPlay loop muted className="myVideo" style={{ width: '100%', height: '100vh', objectFit: 'cover' }}>
+            <source src={takeoffVideo} type="video/mp4" />
+        </video>
     </>
 }
 
